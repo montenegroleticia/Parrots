6979133baf8d8ce75e864d5459a528d0dd75463e
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-01-13 11:49:50 -0300

    Colocar as cartas depois de embaralhar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //Array de cartas
-const cartas = [];
-
+const cartas = 
+["./midia/bobrossparrot.gif", 
+"./midia/bobrossparrot.gif",
+"./midia/explodyparrot.gif", 
+"./midia/explodyparrot.gif",
+"./midia/fiestaparrot.gif", 
+"./midia/fiestaparrot.gif",
+"./midia/metalparrot.gif",
+"./midia/metalparrot.gif",
+"./midia/revertitparrot.gif",
+"./midia/revertitparrot.gif",
+"./midia/tripletsparrot.gif",
+"./midia/tripletsparrot.gif",
+"./midia/unicornparrot.gif",
+"./midia/unicornparrot.gif"];
+const cartasEscolhidas = [];
+//Debugger para controlar pelo console
 debugger
-
 //Perguntando quantas cartas o jogador quer
 let quantidadeCartas = prompt("Com quantas cartas você quer jogar? *de 4-14, apenas números pares*");
 quantidade();
@@ -10,28 +24,52 @@ function quantidade(){
     while (quantidadeCartas < 3 || quantidadeCartas > 14 || quantidadeCartas % 2 !== 0){
         quantidadeCartas = prompt("Com quantas cartas você quer jogar? *de 4-14, apenas números pares*");
     }   
+    criarCartas();
 }
-
-//Inserindo as cartas no jogo
-while(cartas.length == quantidadeCartas){
-    document.querySelector("img");
-    cartas.push(".card");
+//Criar cartas
+function criarCartas(){
+    let indice = 0 
+    while(indice < quantidadeCartas){
+        cartasEscolhidas.push(
+            `<li class="card" onclick="spinCard(this)">
+                <div class="front-face face">
+                    <img src="./midia/back.png" alt="Papagaio verde">
+                </div>
+                <div class="back-face face">
+                    <img src= ${cartas[indice]} alt="Papagaio em gif">
+                </div>
+            </li>`);
+        indice++;
+    }
+    distribuirCartas();
 }
-
-// Embaralhar cartas
-cartas.sort(comparador);
-
-// Esta função pode ficar separada do código acima, onde você preferir
+function distribuirCartas(){
+    cartasEscolhidas.sort(comparador);
+    const ul = document.querySelector("ul");
+    ul.innerHTML = ul.innerHTML + cartasEscolhidas;
+}
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
+/*Virar carta
+function spinCard(girar){
+    alert('oi');
+}*/
 // Recomeçar o jogo
 function recomeço(){
     alert("Você ganhou com %{} jogadas!");
-    const reiniciar = prompt("Deseja reiniciar a partida? *s/n*");
-    if ( reiniciar === "s"){
+    const reiniciar = prompt("Deseja reiniciar a partida? *sim/não*");
+    if ( reiniciar === "sim"){
         quantidadeCartas = prompt("Com quantas cartas você quer jogar? *de 4-14, apenas números pares*");
         quantidade();
     }
 }
+/*
+<li class="card" onclick="spinCard(this)">
+                  <div class="front-face face">
+                    <img src="./midia/back.png" alt="Papagaio verde">
+                  </div>
+                  <div class="back-face face">
+                    <img src="./midia/unicornparrot.gif" alt="Papagaio com cabelo preto">
+                  </div>
+                </li> */
